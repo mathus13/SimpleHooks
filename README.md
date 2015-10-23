8eaf792e2613e5d70c9436dca8714b93301c292e
Shawn Barratt <sbarratt@workflowbydesign.com>
2015-10-23 15:36:55 -0400

    documentation updates

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # SimpleHooks
+
 Simple hook system
+
+Invoke with an associative array containing topic keys with arrays of class (instance of an object) and method
+
+####Example####
+
+	$hooks = array(
+		'\Ethereal\Config\Build' => array(
+			array(
+				'class' => new \Ethereal\Listener(),
+				'method' => 'buildConfig'
+			)
+		)
+	);
+	$hookSystem = new \Ethereal\Hooks($hooks);
+
+The buildConfig hook will be passed an array to parse and return.
--- lib/Hooks.php
+++ lib/Hooks.php
@@ -5,73 +5,98 @@ use Ethereal\Hooks\Exception;
 
 class Hooks
 {
-	protected $hooks;
+    protected $hooks;
 
-	public function __construct(array $hooks = array())
-	{
-		$this->hooks = $hooks;
-	}
+    /**
+     * Instantiate Hooks system with pre-defined listeners
+     * @param array $hooks array of topics => class/method
+     * array(
+     *   '\Ethereal\Config\Build' => array(
+     *       array(
+     *           'class' => new \Ethereal\Listener(),
+     *           'method' => 'buildConfig'
+     *       )
+     *   )
+     * );
+     */
+    public function __construct(array $hooks = array())
+    {
+        $this->hooks = $hooks;
+    }
 
-	/**
-	 * Add a hook listener
-	 * @param string $topic  Topic to subscribe to
-	 * @param object $class  Callable object
-	 * @param string $method Callable method found in object
-	 */
-	public function addListener($topic, $class, $method)
-	{
-		if (!is_string($topic)) {
-			throw new Ethereal\Hooks\Exception('Invalid topic type');
-		}
-		if (!is_string($method)) {
-			throw new Ethereal\Hooks\Exception('Invalid method type');
-		}
-		if (!is_object($class)) {
-			throw new Ethereal\Hooks\Exception('$class must be an instance of an object');
-		}
-		if (!isset($this->hooks[$topic])) {
-			$this->hooks[$topic] = array();
-		}
-		$this->hooks[$topic][] = array(
-			'class' => $class,
-			'method' => $method
-		);
-	}
+    /**
+     * Add a hook listener
+     * @param string $topic  Topic to subscribe to
+     * @param object $class  Callable object
+     * @param string $method Callable method found in object
+     */
+    public function addListener($topic, $class, $method)
+    {
+        if (!is_string($topic)) {
+            throw new Ethereal\Hooks\Exception('Invalid topic type');
+        }
+        if (!is_string($method)) {
+            throw new Ethereal\Hooks\Exception('Invalid method type');
+        }
+        if (!is_object($class)) {
+            throw new Ethereal\Hooks\Exception('$class must be an instance of an object');
+        }
+        if (!isset($this->hooks[$topic])) {
+            $this->hooks[$topic] = array();
+        }
+        $this->hooks[$topic][] = array(
+            'class' => $class,
+            'method' => $method
+        );
+    }
 
-	public function removeListener($topic, $class, $method)
-	{
-		if (isset($this->hooks[$topic])) {
-			foreach ($this->hooks[$topic] as $i => $call) {
-				if (get_class($call['class']) == $class && $call['method'] == $method) {
-					unset($this->hooks[$topic][$i]);
-				}
-			}
-		}
-	}
+    /**
+     * Remove a listener by matching topic.class/listener
+     * @param  string $topic  topic identifier
+     * @param  string $class  class name
+     * @param  string $method listener method
+     * @return null           null
+     */
+    public function removeListener($topic, $class, $method)
+    {
+        if (isset($this->hooks[$topic])) {
+            foreach ($this->hooks[$topic] as $i => $call) {
+                if (get_class($call['class']) == $class && $call['method'] == $method) {
+                    unset($this->hooks[$topic][$i]);
+                }
+            }
+        }
+    }
 
-	public function fire($topic, array $data)
-	{
-		if (!is_string($topic)) {
-			throw new Ethereal\Hooks\Exception('Invalid topic type');
-		}
-		if (!isset($this->hooks[$topic])) {
-			return $data;
-		}
-		foreach ($this->hooks[$topic] as $call) {
-			try {
-				$data = call_user_func(
-					array(
-						$call['class'],
-						$call['method']
-					),
-					$data
-				);
-			} catch (Exception $e) {
-				continue;
-			}
-		}
-		return $data;
-	}
+    /**
+     * Fire a topic with some data.
+     * @param  string $topic Hook Name
+     * @param  array  $data  [description]
+     * @return array         Modified $data
+     */
+    public function fire($topic, array $data)
+    {
+        if (!is_string($topic)) {
+            throw new Ethereal\Hooks\Exception('Invalid topic type');
+        }
+        if (!isset($this->hooks[$topic])) {
+            return $data;
+        }
+        foreach ($this->hooks[$topic] as $call) {
+            try {
+                $data = call_user_func(
+                    array(
+                        $call['class'],
+                        $call['method']
+                    ),
+                    $data
+                );
+            } catch (Exception $e) {
+                continue;
+            }
+        }
+        return $data;
+    }
 
     /**
      * Gets the value of hooks.
